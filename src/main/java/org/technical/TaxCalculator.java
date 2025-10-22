4098e8b302aa8ef9f3e73f7914e0ec17f4df4e50
Hoang4778 <hoang.do0198@gmail.com>
2025-10-22 13:03:26 +0700

    Updated function file

--- src/main/java/org/technical/TaxCalculator.java
+++ src/main/java/org/technical/TaxCalculator.java
@@ -1,21 +1,30 @@
 package org.technical;
 
 public class TaxCalculator {
+
     public static double calculateTax(double income) {
         if (income < 0) {
             throw new IllegalArgumentException("Thu nhập không hợp lệ!");
         }
 
         double rate;
-        if (income <= 5) rate = 0.05;
-        else if (income <= 10) rate = 0.10;
-        else if (income <= 18) rate = 0.15;
-        else if (income <= 32) rate = 0.20;
-        else if (income <= 52) rate = 0.25;
-        else if (income <= 80) rate = 0.30;
-        else rate = 0.35;
+        if (income <= 5) {
+            rate = 0.05;
+        } else if (income <= 10) {
+            rate = 0.10;
+        } else if (income <= 18) {
+            rate = 0.15;
+        } else if (income <= 32) {
+            rate = 0.20;
+        } else if (income <= 52) {
+            rate = 0.25;
+        } else if (income <= 80) {
+            rate = 0.30;
+        } else {
+            rate = 0.35;
+        }
 
-        return income * rate;
+        return income + rate;
     }
 
     public static void main(String[] args) {
